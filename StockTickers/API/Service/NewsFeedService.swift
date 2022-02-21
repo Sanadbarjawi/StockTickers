1e6b2f05f0ca79c064d6fa71ac40477562b61bf5
Sanad.b <Sanadbarj@gmail.com>
2022-02-21 12:20:48 +0200

    services

--- StockTickers/API/Service/NewsFeedService.swift
+++ StockTickers/API/Service/NewsFeedService.swift
@@ -9,11 +9,56 @@ import Foundation
 import Combine
 
 protocol NewsFeedServiceProtocol {
-    //https://raw.githubusercontent.com/dsancov/TestData/main/stocks.csv
     func getNewsFeed() -> AnyPublisher<[News], Error>
 }
 
 final class NewsFeedService: NewsFeedServiceProtocol {
-
-
+    func getNewsFeed() -> AnyPublisher<[News], Error> {
+        var dataTask: URLSessionDataTask?
+        
+        let onSubscription: (Subscription) -> Void = { _ in dataTask?.resume() }
+        let onCancel: () -> Void = { dataTask?.cancel() }
+        
+        // promise type is Result<[Player], Error>
+        return Future<[News], Error> { [weak self] promise in
+            guard let urlRequest = self?.getUrlRequest() else {
+                promise(.failure(ServiceError.urlRequest))
+                return
+            }
+            
+            dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
+                guard let data = data else {
+                    if let error = error {
+                        promise(.failure(error))
+                    }
+                    return
+                }
+                do {
+                    let news = try JSONDecoder().decode([News].self, from: data)
+                    promise(.success(news))
+                } catch {
+                    promise(.failure(ServiceError.decode))
+                }
+            }
+        }
+        .handleEvents(receiveSubscription: onSubscription, receiveCancel: onCancel)
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+        
+    }
+    
+    private func getUrlRequest() -> URLRequest? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "saurav.tech"
+        components.path = "/NewsAPI/everything/cnn.json"
+        
+        guard let url = components.url else { return nil }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.timeoutInterval = 10.0
+        urlRequest.httpMethod = "GET"
+        return urlRequest
+    }
+    
 }
--- StockTickers/API/Service/StocksService.swift
+++ StockTickers/API/Service/StocksService.swift
@@ -8,12 +8,63 @@
 import Foundation
 import Combine
 
+enum ServiceError: Error {
+    case url(URLError)
+    case urlRequest
+    case decode
+}
+
 protocol StocksServiceProtocol {
-    //https://raw.githubusercontent.com/dsancov/TestData/main/stocks.csv
     func getStockTickers() -> AnyPublisher<[Stock], Error>
 }
 
 final class StocksService: StocksServiceProtocol {
-
-
+    func getStockTickers() -> AnyPublisher<[Stock], Error> {
+        var dataTask: URLSessionDataTask?
+        
+        let onSubscription: (Subscription) -> Void = { _ in dataTask?.resume() }
+        let onCancel: () -> Void = { dataTask?.cancel() }
+        
+        // promise type is Result<[Player], Error>
+        return Future<[Stock], Error> { [weak self] promise in
+            guard let urlRequest = self?.getUrlRequest() else {
+                promise(.failure(ServiceError.urlRequest))
+                return
+            }
+            
+            dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
+                guard let data = data else {
+                    if let error = error {
+                        promise(.failure(error))
+                    }
+                    return
+                }
+                do {
+                    let stocks = try JSONDecoder().decode([Stock].self, from: data)
+                    promise(.success(stocks))
+                } catch {
+                    promise(.failure(ServiceError.decode))
+                }
+            }
+        }
+        .handleEvents(receiveSubscription: onSubscription, receiveCancel: onCancel)
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+        
+    }
+    
+    private func getUrlRequest() -> URLRequest? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "raw.githubusercontent.com"
+        components.path = "/dsancov/TestData/main/stocks.csv"
+        
+        guard let url = components.url else { return nil }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.timeoutInterval = 10.0
+        urlRequest.httpMethod = "GET"
+        return urlRequest
+    }
+    
 }
